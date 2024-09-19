c70d3d7946c62dd417a0ba6c8d0bfbb5e34aa03f
damien duport percier <damienviel69@gmail.com>
2024-09-19 12:46:27 +0200

    springBoot + web

---
+++ exercices/dvdstore-core/src/main/java/com/mycompany/dvdstore/repository/file/FileMovieRepository.java
@@ -0,0 +1,101 @@
+package com.mycompany.dvdstore.repository.file;
+
+import com.mycompany.dvdstore.entity.Movie;
+import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class FileMovieRepository implements MovieRepositoryInterface {
+
+    @Value("${movies.file.location}")
+    private File file;
+
+    public void add(Movie movie){
+
+        long lastId=list().stream().map(Movie::getId).max(Long::compare).orElse(0L);
+        movie.setId(lastId+1);
+
+        FileWriter writer;
+        try{
+            writer=new FileWriter(file,true);
+            writer.write(movie.getId()+";"+movie.getTitle()+";"+movie.getGenre()+";"+movie.getDescription()+"\n");
+            writer.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        System.out.println("The movie "+movie.getTitle()+" has been added.");
+    }
+
+    @Override
+    public List<Movie> list() {
+
+        List<Movie> movies=new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            for(String line; (line = br.readLine()) != null; ) {
+                final Movie movie=new Movie();
+                final String[] allProperties = line.split("\\;");
+                movie.setId(Long.parseLong(allProperties[0]));
+                movie.setTitle(allProperties[1]);
+                movie.setGenre(allProperties[2]);
+                movies.add(movie);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.err.println("A movie from the file does not have a proper id");
+            e.printStackTrace();
+        }
+        return movies;
+    }
+
+    @Override
+    public Movie getById(long id) {
+        final Movie movie = new Movie();
+        movie.setId(id);
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            for(String line; (line = br.readLine()) != null; ) {
+
+                final String[] allProperties = line.split("\\;");
+                final long nextMovieId=Long.parseLong(allProperties[0]);
+                if (nextMovieId==id) {
+                    movie.setTitle(allProperties[1]);
+                    movie.setGenre(allProperties[2]);
+                    movie.setDescription(allProperties[3]);
+                    return movie;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.err.println("A movie from the file does not have a proper id");
+            e.printStackTrace();
+        }
+        movie.setTitle("UNKNOWN");
+        movie.setGenre("UNKNOWN");
+        movie.setDescription("UNKNOWN");
+        return movie;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+}
---
+++ exercices/dvdstore-core/src/main/java/com/mycompany/dvdstore/service/MovieServiceInterface.java
@@ -0,0 +1,11 @@
+package com.mycompany.dvdstore.service;
+
+import com.mycompany.dvdstore.entity.Movie;
+
+import java.util.List;
+
+public interface MovieServiceInterface {
+    void registerMovie(Movie movie);
+    List<Movie> getMovieList();
+    Movie getMovieById(long id);
+}
